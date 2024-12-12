b2e3b97d08e559084aec052cc5ff748ed612fe1c
gil-666 <143546119+gil-666@users.noreply.github.com>
2024-12-12 00:56:59 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import { Suspense } from 'vue';
+</script>
+
+<template>
+  <header>
+    <!-- <div class="wrapper"> -->
+    <div class="logo">
+      <h1>GilTube</h1>
+
+    </div>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+    <!-- </div> -->
+    <OverlayBadge value="4" severity="danger" class="inline-flex">
+      <Avatar label="sex" size="large" />
+    </OverlayBadge>
+  </header>
+  <Suspense>
+    <RouterView />
+  </Suspense>
+
+</template>
+
+<style scoped>
+@media (min-width: 1024px) {}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { Button } from 'primevue';
+import Carousel from 'primevue/carousel';
+
+</script>
+
+<template>
+  <main>
+    <div class="container">
+      <h2>Videos</h2><br>
+      <Carousel>
+        
+      </Carousel>
+    </div>
+
+  </main>
+</template>
+
+<script>
+
+</script>
